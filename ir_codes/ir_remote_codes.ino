e528d90bd64677dd16d7f6de71e0e3cfaae943e4
Resinchem <55962781+Resinchem@users.noreply.github.com>
2023-02-06 15:28:43 -0500

    IR Codes updated

---
+++ ir_codes/ir_remote_codes.ino
@@ -0,0 +1,20 @@
+#include <IRremote.h>
+
+const int RECV_PIN = 14;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup(){
+  Serial.begin(115200);
+  irrecv.enableIRIn();
+  //irrecv.blink13(true);
+}
+
+void loop(){
+  if (irrecv.decode()){
+        Serial.print("IR Code Received: ");
+        Serial.println(irrecv.decodedIRData.command);
+        delay(250);
+        irrecv.resume();
+  }
+}
